705bbc2b5aa755f43bf07ac813574157582ad340
zznate <nate@vervewireless.com>
2011-03-21 12:04:51 -0500

    updated to newest version of hector, using HColumnFamily on inserts

--- src/main/java/com/riptano/cassandra/stress/SliceCommand.java
+++ src/main/java/com/riptano/cassandra/stress/SliceCommand.java
@@ -8,6 +8,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.service.CassandraHost;
+import me.prettyprint.cassandra.service.HColumnFamilyImpl;
+import me.prettyprint.hector.api.HColumnFamily;
 import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.beans.ColumnSlice;
 import me.prettyprint.hector.api.factory.HFactory;
@@ -18,12 +21,15 @@ public class SliceCommand extends StressCommand {
     private static Logger log = LoggerFactory.getLogger(SliceCommand.class);
     
     private final SliceQuery<String, String, String> sliceQuery;
+    private final HColumnFamily<String, String> columnFamily;
     
     private static StringSerializer se = StringSerializer.get();
     
     public SliceCommand(int startKey, CommandArgs commandArgs, CommandRunner commandRunner) {
         super(startKey, commandArgs, commandRunner);
         sliceQuery = HFactory.createSliceQuery(commandArgs.keyspace, se, se, se);
+        columnFamily = new HColumnFamilyImpl<String, String>(commandArgs.keyspace, commandArgs.workingColumnFamily, se, se);
+        columnFamily.setCount(commandArgs.columnCount);
     }
 
     @Override
@@ -32,13 +38,17 @@ public class SliceCommand extends StressCommand {
         Random random = new Random();
         sliceQuery.setColumnFamily(commandArgs.workingColumnFamily);
         log.debug("Starting SliceCommand");
-        try {
+        try {            
             while (rows < commandArgs.getKeysPerThread()) {
-                sliceQuery.setKey(String.format("%010d", startKey + random.nextInt(commandArgs.getKeysPerThread())));
-                sliceQuery.setRange(null, null, false, commandArgs.columnCount);
-                QueryResult<ColumnSlice<String,String>> result = sliceQuery.execute();
-                LatencyTracker readCount = commandRunner.latencies.get(result.getHostUsed());
-                readCount.addMicro(result.getExecutionTimeMicro());
+                long nanos = System.nanoTime();
+                columnFamily.addKey(String.format("%010d", startKey + random.nextInt(commandArgs.getKeysPerThread())));
+                //sliceQuery.setKey(String.format("%010d", startKey + random.nextInt(commandArgs.getKeysPerThread())));
+                //sliceQuery.setRange(null, null, false, commandArgs.columnCount);
+                //QueryResult<ColumnSlice<String,String>> result = sliceQuery.execute();
+                columnFamily.getColumns();
+                //LatencyTracker readCount = commandRunner.latencies.get(new CassandraHost("localhost:9160"));
+                ///readCount.addMicro((System.nanoTime() - nanos) / 1000);
+                columnFamily.removeKeys().clear();                
                 rows++;
             }
         } catch (Exception e) {
